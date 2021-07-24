90e18d01a0d1733690f8b7868d1bffbaf503e815
ss2200hz <ss2200hz@163.com>
2021-07-24 11:35:20 +0800

    优化代码结构，加入全局变量修改。加入获胜判定

--- venv/game_demo.py
+++ venv/game_demo.py
@@ -4,13 +4,31 @@ import Room,Road,Player,Consts,MoveController
 import pygame
 
 isPause = False
+isOver = False
 
 def resetGame():
-    roomList = []
-    roadList = []
-    player = None
-    startRoom = None
-    endRoom = None
+    global roomList
+    global roadList
+    global player
+    global startRoom
+    global endRoom
+    global isOver
+    isOver = False
+    roomList, startRoom, endRoom = Room.createMap()  # 初始化地图
+    roadList = Road.createVLTypeRoad(startRoom, endRoom)
+    player = Player.Player()  # 初始化玩家
+    player.roomCoor = startRoom.Coor
+
+
+def gameOver():
+    if player.roomCoor == endRoom.Coor:
+        global isOver
+        isOver = True
+        final_text = "Your Win"
+        ft2_font = pygame.font.SysFont("Arial", 50)  # 设置第二行文字字体
+        ft2_surf = font.render(final_text, 1, (253, 177, 6))  # 设置第二行文字颜色
+        screen.blit(ft2_surf, [screen.get_width() / 2 - ft2_surf.get_width() / 2, 200])  # 设置第二行文字显示位置
+        pygame.display.flip()  # 更新整个待显示的Surface对象到屏幕上
 
 #帧函数，显示更新
 def update():
@@ -31,6 +49,7 @@ if __name__ == '__main__':
     pygame.init()                       # 初始化pygame
     size = width, height = 640, 480     # 设置窗口大小
     screen = pygame.display.set_mode(size)  # 显示窗口
+    font = pygame.font.SysFont("Arial", 50)  # 设置字体和大小
     roomList, startRoom, endRoom = Room.createMap()  # 初始化地图
     # roadList = Road.createHLTypeRoad(startRoom, endRoom)#初始化道路
     roadList = Road.createVLTypeRoad(startRoom, endRoom)
@@ -49,28 +68,26 @@ if __name__ == '__main__':
                         isPause = True
                 if event.key == pygame.KSCAN_J:#Enter重置游戏
                     resetGame()
-                    roomList, startRoom, endRoom = Room.createMap()  # 初始化地图
-                    roadList = Road.createVLTypeRoad(startRoom, endRoom)
-                    player = Player.Player()  # 初始化玩家
-                    player.roomCoor = startRoom.Coor
-            if not isPause:
-                if event.type == pygame.KEYDOWN:#移动角色
-                    if event.key == pygame.K_LEFT:
-                        moveStatus = True
-                        moveDirection = Consts.LEFT_DIRECTION
-                    if event.key == pygame.K_RIGHT:
-                        moveStatus = True
-                        moveDirection = Consts.RIGHT_DIRECTION
-                    if event.key == pygame.K_UP:
-                        moveStatus = True
-                        moveDirection = Consts.UP_DIRECTION
-                    if event.key == pygame.K_DOWN:
-                        moveStatus = True
-                        moveDirection = Consts.DOWN_DIRECTION
-                    if moveStatus:
-                        if MoveController.isPlayerCanMove(moveDirection=moveDirection,
-                                                          roadList=roadList,
-                                                          player=player):
-                            MoveController.moveCharacter(character=player,direction=moveDirection)
+            if not isOver:
+                if not isPause:
+                    if event.type == pygame.KEYDOWN:#移动角色
+                        if event.key == pygame.K_LEFT:
+                            moveStatus = True
+                            moveDirection = Consts.LEFT_DIRECTION
+                        if event.key == pygame.K_RIGHT:
+                            moveStatus = True
+                            moveDirection = Consts.RIGHT_DIRECTION
+                        if event.key == pygame.K_UP:
+                            moveStatus = True
+                            moveDirection = Consts.UP_DIRECTION
+                        if event.key == pygame.K_DOWN:
+                            moveStatus = True
+                            moveDirection = Consts.DOWN_DIRECTION
+                        if moveStatus:
+                            if MoveController.isPlayerCanMove(moveDirection=moveDirection,
+                                                              roadList=roadList,
+                                                              player=player):
+                                MoveController.moveCharacter(character=player,direction=moveDirection)
                 update()
+        gameOver()
     pygame.quit()  # 退出pygame
